ce12d2c03e13bc0dca820f78a4320415b3f6e45f
BHedi89 <hedi.besenyei89@gmail.com>
2021-06-08 13:27:54 +0200

    add quote page detail

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,6 +1,14 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+    {id: 'q1', author: 'Max', text: 'Learning React is fun!'},
+    {id: 'q2', author: 'Maximilian', text: 'Learnin React is grest!'}
+];
+
+
 const AllQuotes = () => {
     return (
-        <h1>All Quotes Page</h1>
+        <QuoteList quotes={DUMMY_QUOTES} />
     )
 
 }
